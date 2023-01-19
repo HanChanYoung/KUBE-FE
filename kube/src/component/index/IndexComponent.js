3c6373aaec6d748e3706781fa0314c405a6c9703
JMINJong <wjsalswhd55@gachon.ac.kr>
2023-01-19 13:21:36 +0900

    ADD download image from cloud

--- kube/src/component/index/IndexComponent.js
+++ kube/src/component/index/IndexComponent.js
@@ -1,24 +1,31 @@
 import { Button } from "@mui/material";
-import { IMAGE_DOWNLOAD } from "../../config";
+import { IMAGE_DOWNLOAD,UPLOAD_IMAGE } from "../../config";
 import Header from "../public/Header";
 import { useState,} from "react";
 import { Buffer } from "buffer";
+import Swal from 'sweetalert2'
 
 
 export default function IndexComponent(){
 
-    const [img,setImg]=useState('');
+    const [img,setImg]=useState(null);
     return(
         <div style={{width:"100%"}}>
             <Header></Header>
             <Button
-            onClick={()=>{
-                const data=IMAGE_DOWNLOAD();
-                let base64ImageString = Buffer.from(data).toString('base64')
-                setImg(base64ImageString);
+            onClick={async()=>{
+                const data= await IMAGE_DOWNLOAD();
+                const imgSrc=URL.createObjectURL(data)
+                console.log(imgSrc)
+                setImg(imgSrc);                
             }}
-            >test</Button>
-            <img src={`data:image/jpeg;base64,${img}`} />
+            >download</Button>
+            <img src={img} alt="img" />
+            <Button
+            onClick={()=>{
+                UPLOAD_IMAGE();
+            }}
+            >upload</Button>
         </div>
     )
 }
